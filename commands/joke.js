601dc263930e66ecdde7000fe146b0a9c6ed7555
unknown <kenshimadax@gmail.com>
2026-04-05 23:25:29 +0800

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -3,17 +3,17 @@ const axios = require('axios');
 const { sendMessage } = require('../handles/sendMessage');
 
 module.exports = {
-    name: ['joke', 'funny', 'laugh'],
+    name: ['joke'],
     usage: 'joke',
     version: '1.0.0',
     author: 'AutoPageBot',
-    category: 'others',
+    category: 'fun',
     cooldown: 0,
 
     async execute(senderId, args, pageAccessToken) {
         try {
             const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
-            const joke = `${response.data.setup}\n\n${response.data.punchline}`;
+            const joke = `𝗥𝗮𝗻𝗱𝗼𝗺 𝗝𝗼𝗸𝗲:n\n\${response.data.setup}\n\n${response.data.punchline}`;
             await sendMessage(senderId, { text: joke }, pageAccessToken);
         } catch (error) {
             await sendMessage(senderId, { text: '❌ No joke found. Try again!' }, pageAccessToken);
