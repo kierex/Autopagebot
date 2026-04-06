0721586e8946f63d63df453a77edcfb444c6c61d
unknown <kenshimadax@gmail.com>
2026-04-06 15:50:16 +0800

    Update joke.js

--- commands/joke.js
+++ commands/joke.js
@@ -13,7 +13,7 @@ module.exports = {
     async execute(senderId, args, pageAccessToken) {
         try {
             const response = await axios.get('https://official-joke-api.appspot.com/random_joke');
-            const joke = `𝗥𝗮𝗻𝗱𝗼𝗺 𝗝𝗼𝗸𝗲:n\n\${response.data.setup}\n\n${response.data.punchline}`;
+            const joke = `𝗥𝗮𝗻𝗱𝗼𝗺 𝗝𝗼𝗸𝗲:n\n\ ${response.data.setup}\n\n${response.data.punchline}`;
             await sendMessage(senderId, { text: joke }, pageAccessToken);
         } catch (error) {
             await sendMessage(senderId, { text: '❌ No joke found. Try again!' }, pageAccessToken);
